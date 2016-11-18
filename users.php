50dca8f8cf4e33a5933bb18b66e8a400fba987d8
Saeid Pournejati <pournejati@gmail.com>
2016-11-18 15:05:35 -0600

    Updated Users Page Layout.

--- users.php
+++ users.php
@@ -8,133 +8,92 @@
         <h1>Current Users</h1>
     </div>
 
-    <p>
-        Many of the world’s most dynamic healthcare institutions, academic labs, and NLP users find CLAMP 
-        make their annotation and NLP tasks more effective. 
-        Over 100 users in more than 50 sites already use CLAMP.
-    </p>
-    <p>These are some of the CLAMP users actively using CLAMP for their NLP and Machine Learning tasks.</p>
+    <div>
+        <p>
+            Many of the world’s most dynamic healthcare institutions, academic labs, and NLP users find CLAMP 
+            make their annotation and NLP tasks more effective. 
+            Over 100 users in more than 50 sites already use CLAMP.
+        </p>
+        <p>These are some of the CLAMP users actively using it for their NLP and Machine Learning tasks.</p>
+    </div>
 
 
     <div id="customers-logo" class="row">
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <img src="img/clients/UTH.jpg" alt="University of Texas Health Science Center." class="thumbnail"/>
         </div>
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/UTH-SBMI.jpg" alt="University of Texas Health Science Center School of Biomedical Informatics." class="thumbnail"/>
-        </div>
-    </div>
-    <div id="customers-logo" class="row">
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/MDAnderson.jpeg" alt="MD Anderson Cancer Center" class="thumbnail"/>
-        </div>
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/BaylorCollegeofMedicine.jpg" alt="Baylor College of Medicine" class="thumbnail"/>
-        </div>
-    </div>
-    <div id="customers-logo" class="row">
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/ApolloHIT.png" alt="Apollo HIT" class="thumbnail"/>
-        </div>
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/AssurerxHealthInc.jpg" alt="Assurerx Health Inc" class="thumbnail"/>
-        </div>
-    </div>
-    <div id="customers-logo" class="row">
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/BERGAnalytics.png" alt="BERG Analytics" class="thumbnail"/>
-        </div>
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <img src="img/clients/BERGHealth.png" alt="BERG Health" class="thumbnail"/>
         </div>
-    </div>
-    <div id="customers-logo" class="row">
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
+            <img src="img/clients/MDAnderson.jpeg" alt="MD Anderson Cancer Center" class="thumbnail"/>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <img src="img/clients/BaylorCollegeofMedicine.jpg" alt="Baylor College of Medicine" class="thumbnail"/>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <img src="img/clients/ApolloHIT.png" alt="Apollo HIT" class="thumbnail"/>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <img src="img/clients/AssurerxHealthInc.jpg" alt="Assurerx Health Inc" class="thumbnail"/>
+        </div>
+        <div class="col-md-4 col-sm-6">
             <img src="img/clients/CaseWesternReverseUniversity.png" alt="Case Western Reverse University" class="thumbnail"/>
         </div>
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <img src="img/clients/ColumbiaUniversity.png" alt="Columbia University" class="thumbnail"/>
         </div>
-    </div>
-    <div id="customers-logo" class="row">
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/CIC-IPN.jpg" alt="CIC-IPN" class="thumbnail"/>
-        </div>
-
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <img src="img/clients/CUHK.jpg" alt="CUHK" class="thumbnail"/>
-        </div>
-    </div>
-    <div id="customers-logo" class="row">        
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/NCHHealth.jpg" alt="NCH Health" class="thumbnail"/>
-        </div>
-        <div class="col-md-6 col-sm-6">
+        </div>        
+        <div class="col-md-4 col-sm-6">
             <img src="img/clients/MCGHealthLLC.jpg" alt="MCG Health LLC" class="thumbnail"/>
         </div>
-    </div>
-    <div id="customers-logo" class="row">
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
+            <img src="img/clients/NCHHealth.jpg" alt="NCH Health" class="thumbnail"/>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <img src="img/clients/CIC-IPN.jpg" alt="CIC-IPN" class="thumbnail"/>
+        </div>
+        <div class="col-md-4 col-sm-6">
             <img src="img/clients/JiangsuProvinceHospital.jpg" alt="Jiangsu Province Hospital" class="thumbnail"/>
         </div>
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <img src="img/clients/NorthwesternUniversity.png" alt="Northwestern University" class="thumbnail"/>
         </div>
-    </div>
-    <div id="customers-logo" class="row">
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <img src="img/clients/SichuanUniversity.png" alt="Sichuan University" class="thumbnail"/>
         </div>
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <img src="img/clients/StanfordUniversity.png" alt="Stanford University" class="thumbnail"/>
         </div>
-    </div>
-    <div id="customers-logo" class="row">
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/StanfordUniversityNLPGroup.jpg" alt="Stanford University NLP Group" class="thumbnail"/>
-        </div>
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/UniversityOfMichigan.jpg" alt="University Of Michigan" class="thumbnail"/>
-        </div>
-
-    </div>
-    <div id="customers-logo" class="row">
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/UniversityOfHeidelberg.png" alt="University Of Heidelberg" class="thumbnail"/>
-        </div>
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/UniversityOfMarylandBaltimoreCounty.png" alt="University Of Maryland Baltimore County" class="thumbnail"/>
-        </div>
-    </div>
-    <div id="customers-logo" class="row">
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/UCSDBioinformatics.png" alt="UCSD Bioinformatics" class="thumbnail"/>
-        </div>
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/UniversityOfMinnesota.png" alt="University Of Minnesota" class="thumbnail"/>
-        </div>
-    </div>
-    <div id="customers-logo" class="row">
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/UniversityOfUtah.png" alt="University Of Utah" class="thumbnail"/>
-        </div>
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/UPMC.jpg" alt="UPMC" class="thumbnail"/>
-        </div>
-    </div>
-    <div id="customers-logo" class="row">
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/USF.jpg" alt="USF" class="thumbnail"/>
-        </div>
-        <div class="col-md-6 col-sm-6">
-            <img src="img/clients/VanderbiltUniversity.jpg" alt="Vanderbilt University" class="thumbnail"/>
-        </div>
-    </div>
-    <div id="customers-logo" class="row">
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <img src="img/clients/VirginiaCommonwealthUniversity.jpg" alt="Virginia Commonwealth University" class="thumbnail"/>
         </div>
-
+        <div class="col-md-4 col-sm-6">
+            <img src="img/clients/USF.jpg" alt="USF" class="thumbnail"/>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <img src="img/clients/UniversityOfHeidelberg.png" alt="University Of Heidelberg" class="thumbnail"/>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <img src="img/clients/UniversityOfMarylandBaltimoreCounty.png" alt="University Of Maryland Baltimore County" class="thumbnail"/>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <img src="img/clients/UCSDBioinformatics.png" alt="UCSD Bioinformatics" class="thumbnail"/>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <img src="img/clients/UniversityOfMinnesota.png" alt="University Of Minnesota" class="thumbnail"/>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <img src="img/clients/UniversityOfUtah.png" alt="University Of Utah" class="thumbnail"/>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <img src="img/clients/UPMC.jpg" alt="UPMC" class="thumbnail"/>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <img src="img/clients/VanderbiltUniversity.jpg" alt="Vanderbilt University" class="thumbnail"/>
+        </div>
     </div>
 
 </div>
