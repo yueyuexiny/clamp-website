5ae0e4f7268e60b15d4189f75446e78321af7950
Ruiling Liu <Ruiling.Liu@uth.tmc.edu>
2017-01-25 13:24:28 -0600

    Update the publication page

--- publications.php
+++ publications.php
@@ -2,12 +2,6 @@
 <?php $pageDescription = "Natural Language Processing Tool Documentation. Clamp Current Users." ?>
 <?php include_once dirname(__FILE__) . '/views/header.php'; ?>
 
-
-
-
-
-
-
 <div class="container page-container">
     <br/>
     <div class="section-header">
@@ -24,10 +18,10 @@
 
         <div class="list-group list-group-root well">
 
-            <a href="#" class="list-group-item list-header" >Named entity recognition </a>
+            <p class="list-group-item list-header" >NLP Task: Named entity recognition </p>
             <div class="list-group">
 
-                <a class="list-group-item"> <i class="fa fa-trophy fa-1x" aria-hidden="true"></i>2009 i2b2, medication <span class="badge">#2</span>
+                <a class="list-group-item list-sub-header">2009 i2b2, medication <span class="badge badge-pill badge-success"><i class="fa fa-trophy fa-1x" aria-hidden="true" style="margin-right: 5px"></i> #2</span>
                 </a>
                 <div class="list-group">
 
@@ -35,13 +29,13 @@
                     </a>
                 </div>
 
-                <a href="#" class="list-group-item"><i class="fa fa-trophy fa-1x" aria-hidden="true"></i> 2010 i2b2 problem, treatment, test <span class="badge">#2</span></a>
+                <a href="#" class="list-group-item list-sub-header"> 2010 i2b2 problem, treatment, test <span class="badge"><i class="fa fa-trophy fa-1x" aria-hidden="true" style="margin-right: 5px"></i> #2</span></a>
                 <div class="list-group">
                     <a href="https://www.ncbi.nlm.nih.gov/pubmed/21508414" class="list-group-item" target="_blank"> <span class="glyphicon glyphicon-file"></span>Jiang M Chen Y Liu Met al. A study of machine-learning-based approaches to extract clinical entities and their assertions from discharge summaries. J Am Med Inform Assoc  2011;18:601–6.
                     </a>
                 </div>
 
-                <a href="#" class="list-group-item"><i class="fa fa-trophy fa-1x" aria-hidden="true"></i> 2013 SHARe/CLEF abbreviation <span class="badge">#1</span></a>
+                <a href="#" class="list-group-item list-sub-header"> 2013 SHARe/CLEF abbreviation <span class="badge"><i class="fa fa-trophy fa-1x" aria-hidden="true" style="margin-right: 5px"></i> #1</span></a>
                 <div class="list-group">
                     <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3168315/" class="list-group-item" target="_blank"> <span class="glyphicon glyphicon-file"></span>Tang B, Wu Y, Jiang M, and Xu H. A Machine Learning based System for Disorder Concept Extraction. 2013. Proceedings of CLEF 2013 Evaluation Labs and Workshop.</a>
                     <a href="http://ceur-ws.org/Vol-1179/CLEF2013wn-CLEFeHealth-WuEt2013.pdf" class="list-group-item" target="_blank"> <span class="glyphicon glyphicon-file"></span>Wu Y, Tang B, Jiang M, Moon S, Denny JC, Xu H. Clinical Acronym/Abbreviation Normalization using a Hybrid Approach. 2013. Proceedings of CLEF 2013 Evaluation Labs and Workshop.
@@ -50,30 +44,30 @@
 
             </div>
 
-            <a href="#" class="list-group-item list-header">Relation extraction</a>
+            <p class="list-group-item list-header">NLP Task: Relation extraction</p>
             <div class="list-group">
 
-                <a href="#" class="list-group-item"><i class="fa fa-trophy fa-1x" aria-hidden="true"></i> 2012 i2b2 Temporal <span class="badge">#1</span></a>
+                <a href="#" class="list-group-item list-sub-header"> 2012 i2b2 Temporal <span class="badge"><i class="fa fa-trophy fa-1x" aria-hidden="true" style="margin-right: 5px"></i> #1</span></a>
                 <div class="list-group">
                     <a href="https://www.ncbi.nlm.nih.gov/pubmed/23571849" class="list-group-item" target="_blank"> <span class="glyphicon glyphicon-file"></span>Tang B, Wu Y, Jiang M, Chen Y, Denny JC, Xu H. A hybrid system for temporal information extraction from clinical text. J Am Med Inform Assoc. 2013.
                     </a>
                 </div>
 
-                <a href="#" class="list-group-item"><i class="fa fa-trophy fa-1x" aria-hidden="true"></i> 2015 SemEval Disease-modifier <span class="badge">#1</span>
+                <a href="#" class="list-group-item list-sub-header"> 2015 SemEval Disease-modifier <span class="badge"><i class="fa fa-trophy fa-1x" aria-hidden="true" style="margin-right: 5px"></i>#1</span>
                 </a>
                 <div class="list-group">
                     <a href="https://pdfs.semanticscholar.org/2a6f/5f0da0b833b135731245e755f71bc12645cf.pdf" target="_blank" class="list-group-item"> <span class="glyphicon glyphicon-file"></span>Xu J, Zhang Y, Wang J, Wu Y, Jiang M, Soysal E, and Xu H. UTH-CCB: The Participation of the SemEval 2015 Challenge – Task 14. Proceedings of the 9th International Workshop on Semantic Evaluation (SemEval 2015). Denver, Colorado. 2015:311-314.
                     </a>
                 </div>
 
-                <a href="#" class="list-group-item"><i class="fa fa-trophy fa-1x" aria-hidden="true"></i> 2015 BioCREATIVE Chemical-induced disease
+                <a href="#" class="list-group-item list-sub-header">2015 BioCREATIVE Chemical-induced disease <span class="badge"><i class="fa fa-trophy fa-1x" aria-hidden="true" style="margin-right: 5px"></i> #1</span>
                 </a>
                 <div class="list-group">
                     <a href="http://www.biocreative.org/media/store/files/2015/BCV2015_paper_38n.pdf" target="_blank" class="list-group-item"> <span class="glyphicon glyphicon-file"></span>Xu J, Wu Y, Zhang Y, Wang J, Liu R, Wei Q, and Xu H. UTH-CCB@BioCreative V CDR Task: Identifying Chemical-induced Disease Relations in Biomedical Text. Proceedings of the Fifth BioCreative Challenge Evaluation Workshop, 2015:254-259.
                     </a>
                 </div>
 
-                <a href="#" class="list-group-item"><i class="fa fa-trophy fa-1x" aria-hidden="true"></i> 2016 SemEvel, Temporal <span class="badge">#1</span>
+                <a href="#" class="list-group-item list-sub-header"> 2016 SemEvel, Temporal <span class="badge"><i class="fa fa-trophy fa-1x" aria-hidden="true" style="margin-right: 5px"></i>#1</span>
                 </a>
                 <div class="list-group">
                     <a href="https://www.aclweb.org/anthology/S/S16/S16-1201.pdf" target="_blank" class="list-group-item"> <span class="glyphicon glyphicon-file"></span>Lee H, Zhang Y, Xu J, Moon S, Wang J, Wu Y, and Xu H, UTHealth at SemEval-2016 Task 12: an End-to-End System for Temporal Information Extraction from Clinical Notes. Proceedings of the 10th International Workshop on Semantic Evaluation (SemEval-2016), 1292-1297, San Diego, California, June, 2016.
@@ -84,20 +78,22 @@
             </div>
 
 
-            <a href="#" class="list-group-item list-header">UMLS encoding</a>
+            <p class="list-group-item list-header">NLP Task: UMLS encoding</p>
             <div class="list-group">
 
-                <a href="#" class="list-group-item"><i class="fa fa-trophy fa-1x" aria-hidden="true"></i> 2014 SemEval, disorder <span class="badge">#1</span></a>
+                <a href="#" class="list-group-item list-sub-header"> 2014 SemEval, disorder <span class="badge"><i class="fa fa-trophy fa-1x" aria-hidden="true" style="margin-right: 5px"></i>#1</span>
+                </a>
                 <div class="list-group">
                     <a href="" class="list-group-item" target="_blank"> <span class="glyphicon glyphicon-file"></span>Zhang Y, Wang J, Tang B, Wu Y, Jiang M, Chen Y, Xu H. UTH_CCB: A Report for SemEval 2014 – Task 7 Analysis of Clinical Text. SemEval 2014, 802–806, Dublin, Ireland, August 23-24, 2014.
                     </a>
                 </div>
             </div>
 
-            <a href="#" class="list-group-item list-header">De-identification</a>
+            <p class="list-group-item list-header">NLP Task: De-identification</p>
             <div class="list-group">
 
-                <a href="#" class="list-group-item"><i class="fa fa-trophy fa-1x" aria-hidden="true"></i>2016 CEGS N-GRID, de-identification <span class="badge">#2</span></a>
+                <a href="#" class="list-group-item list-sub-header">2016 CEGS N-GRID, de-identification <span class="badge"><i class="fa fa-trophy fa-1x" aria-hidden="true" style="margin-right: 5px"></i> #2</span>
+                </a>
             </div>
 
 
