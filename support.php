08fbd64debfa63cd9afde3e42d6a42a150caa963
Ruiling Liu <Ruiling.Liu@uth.tmc.edu>
2017-01-31 11:48:58 -0600

    Update the support page

--- support.php
+++ support.php
@@ -1,21 +1,41 @@
 <?php $pageTitle = "CLAMP Current Users" ?>
 <?php $pageDescription = "Natural Language Processing Tool Documentation. Clamp Current Users." ?>
 <?php include_once dirname(__FILE__) . '/views/header.php'; ?>
-
-    <div class="container page-container">
-        <br/>
-        <div class="section-header">
-            <h1>CLAMP Support</h1>
+    <div class="container">
+        <div class="page-header">
+            <h1 class="text-center">CLAMP Support</h1>
         </div>
+        <p class="lead text-center"></p>
+        <div class="container">
+            <div class="row stylish-panel">
+                <div class="col-md-4">
+                    <div>
+                        <img height="200" src="img/github.png" alt="CLAMP GitHub Page" class="img-circle img-thumbnail" style="margin-bottom: 20px">
+                        <h3>Discussion Forums</h3>
+                        <p class="p-left">Find and share solutions and ideas with your peers and CLAMP experts on our   <a href="https://github.com/UTHealth-CCB/clamp-support/issues" target="_blank" >GitHub page </a>.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div>
+                        <img src="img/phone.png" alt="CLAMP Telephone number" class="img-circle img-thumbnail" style="margin-bottom: 20px">
+                        <h3>Telephone</h3>
+                        <p class="p-left">
+                            9:00 a.m. - 5:00 p.m. Central Time<br>
+                            Telephone :713-500-3619</p>
 
-        <div style="margin-top: 150px;margin-bottom: 150px">
-            <h3>Coming Soon</h3>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div>
+                        <img src="img/email.png" alt="CLAMP Emaill Address" class="img-circle img-thumbnail" style="margin-bottom: 20px">
+                        <h3>Email</h3>
+                        <p class="p-left">For more detailed information, please refer to our Contact Us section below
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-
     </div>
-
-
-
-
-
+    <!-- /container -->
 <?php include_once dirname(__FILE__) . '/views/footer.php'; ?>
