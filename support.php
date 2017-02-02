a8e85064e783aa45cf5aa4582c128354ed1ef7b4
Ruiling Liu <Ruiling.Liu@uth.tmc.edu>
2017-02-02 16:57:54 -0600

    Add office hour on support page

--- support.php
+++ support.php
@@ -8,29 +8,43 @@
         <p class="lead text-center">CLAMP development and maintenance are supported by institutional funds. We will provide technical support to the license holders. For academic license holders, you will get limited email and phone support. For commercial license holders, you will get sufficient support to get CLAMP up and running.</p>
         <div class="container">
             <div class="row stylish-panel">
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <div>
                         <img height="200" src="img/github.png" alt="CLAMP GitHub Page" class="img-circle img-thumbnail" style="margin-bottom: 20px">
-                        <h3>Discussion Forum</h3>
+                        <h4>Discussion Forum</h4>
                         <p class="p-left">Find and share solutions and ideas with your peers and CLAMP experts on our   <a href="https://github.com/UTHealth-CCB/clamp-support/issues" target="_blank" >GitHub page </a>.
                         </p>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <div>
                         <img src="img/phone.png" alt="CLAMP Telephone number" class="img-circle img-thumbnail" style="margin-bottom: 20px">
-                        <h3>Telephone</h3>
+                        <h4>Telephone</h4>
                         <p class="p-left">
                             9:00 a.m. - 5:00 p.m. Central Time<br>
                             Telephone :713-500-3619</p>
 
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <div>
                         <img src="img/email.png" alt="CLAMP Emaill Address" class="img-circle img-thumbnail" style="margin-bottom: 20px">
-                        <h3>Email</h3>
+                        <h4>Email</h4>
                         <p class="p-left">For more detailed information, please refer to our Contact Us section below
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div>
+                        <img src="img/office.png" alt="CLAMP Office Hour" class="img-circle img-thumbnail" style="margin-bottom: 20px">
+                        <h4>Office Hour</h4>
+                        <p class="p-left">Please join my meeting from your computer, tablet or smartphone.
+                            <a href=" https://global.gotomeeting.com/join/379685837">GoToMeeting</a><br><br>
+                            You can also dial in using your phone.<br>
+                            United States: +1 (872) 240-3311<br>
+                            Access Code: 379-685-837<br><br>
+                            First GoToMeeting? Try a <a href="http://help.citrix.com/getready">test session</a>
                         </p>
                     </div>
                 </div>
