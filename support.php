ae7877813fb35453b1e5ccfb2e2b9df622cb2011
Ruiling Liu <Ruiling.Liu@uth.tmc.edu>
2017-02-03 13:02:16 -0600

    Update the support page

--- support.php
+++ support.php
@@ -8,7 +8,7 @@
         <p class="lead text-center">CLAMP development and maintenance are supported by institutional funds. We will provide technical support to the license holders. For academic license holders, you will get limited email and phone support. For commercial license holders, you will get sufficient support to get CLAMP up and running.</p>
         <div class="container">
             <div class="row stylish-panel">
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div>
                         <img height="200" src="img/github.png" alt="CLAMP GitHub Page" class="img-circle img-thumbnail" style="margin-bottom: 20px">
                         <h4>Discussion Forum</h4>
@@ -16,7 +16,7 @@
                         </p>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div>
                         <img src="img/phone.png" alt="CLAMP Telephone number" class="img-circle img-thumbnail" style="margin-bottom: 20px">
                         <h4>Telephone</h4>
@@ -26,12 +26,11 @@
 
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div>
                         <img src="img/email.png" alt="CLAMP Emaill Address" class="img-circle img-thumbnail" style="margin-bottom: 20px">
                         <h4>Email</h4>
                         <p class="p-left">
-                            <strong>Research Coordinator</strong><br>
                             Anupama E. Gururaj, PhD<br>
                             <span class="glyphicon glyphicon-envelope"></span>
                             <a href="mailto:Anupama.E.Gururaj@uth.tmc.edu">anupama.e.gururaj@uth.tmc.edu</a>
@@ -40,23 +39,30 @@
                     </div>
                 </div>
 
-                <div class="col-md-3">
-                    <div>
-                        <img src="img/office.png" alt="CLAMP Office Hour" class="img-circle img-thumbnail" style="margin-bottom: 20px">
-                        <h4>Office Hour</h4>
-                        <p class="p-left">
-                            We will have  a CLAMP office hour on Mondays, every alternating week starting February 27th. The call-in information is provided below:<br><br>
-                            Please join the meeting from your computer, tablet or smartphone.
-                            <a target="_blank" href=" https://global.gotomeeting.com/join/379685837">GoToMeeting</a><br><br>
-                            You can also dial in using your phone.<br>
-                            United States: +1 (872) 240-3311<br>
-                            Access Code: 379-685-837<br><br>
-                            First GoToMeeting? Try a <a target="_blank" href="http://help.citrix.com/getready">test session</a>
-                        </p>
+
+            </div>
+
+
+            <div class="container">
+                <div class="row stylish-panel">
+                    <div class="col-md-12">
+                        <div style="text-align: center">
+                            <img src="img/office.png" alt="CLAMP Office Hour" class="img-circle img-thumbnail" style="margin-bottom: 20px">
+                            <h4>Office Hour</h4>
+                            <p class="p-left">
+                                We will have  a CLAMP office hour on Mondays, every alternating week starting February 27th. The call-in information is provided below:<br><br>
+                                Please join the meeting from your computer, tablet or smartphone.
+                                <a target="_blank" href=" https://global.gotomeeting.com/join/379685837">GoToMeeting</a><br><br>
+                                You can also dial in using your phone.<br>
+                                United States: +1 (872) 240-3311<br>
+                                Access Code: 379-685-837<br><br>
+                                First GoToMeeting? Try a <a target="_blank" href="http://help.citrix.com/getready">test session</a>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
+                </div>
     </div>
+        </div>
     <!-- /container -->
 <?php include_once dirname(__FILE__) . '/views/footer.php'; ?>
