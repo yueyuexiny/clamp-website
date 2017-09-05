5ad2d68c34aa27c0b142c3cdfeabcdbe2d5ca07e
ftiryaki <firat.tiryaki@uth.tmc.edu>
2017-09-05 15:25:34 -0500

    demotext

--- clampdemo.php
+++ clampdemo.php
@@ -1,15 +1,21 @@
 <?php $pageTitle = "CLAMP Current Users" ?>
 <?php $pageDescription = "Natural Language Processing Tool Documentation. Clamp Current Users." ?>
 <?php include_once dirname(__FILE__) . '/views/header.php'; ?>
-
-    <div class="container" align="center">
+	<div class="container" align="center">
         <div class="page-header">
             <h1 class="text-center">CLAMP Demo</h1>
+        
         </div>
-        
-        
-        <form action="http://localhost:9999/clampdemo" method="POST">
-        <textarea type="text" cols="86" rows="20" name="name"/>Sample Type / Medical Specialty:  Discharge Summary
+        <center>
+        <script>
+        function submitForm(){
+        	document.demoForm.submit();
+
+            
+        }
+        </script>
+        <form action="http://localhost:9999/clampdemo" method="POST" target="demoFrame" name="demoForm">
+        <textarea cols="50" rows="20" name="demotext">Sample Type / Medical Specialty:  Discharge Summary
 Sample Name: Psychiatric Discharge Summary - 1
 Description:  Discharge summary of a patient with mood swings and oppositional and defiant behavior.
 (Medical Transcription Sample Report)
@@ -36,11 +42,19 @@ DISCHARGE PLAN:    As recommended by the treatment team, the patient was dischar
 DISCHARGE INSTRUCTIONS/MEDICATIONS:  The patient is to continue treatment at North Star RTC.  Discharge medications are Adderall XR 30 mg p.o. a.m., Depakote 250 mg p.o. t.i.d., Zoloft 55 mg p.o. q. a.m.  The prescription was given to the patient's representative.  As other instructions, the patient may continue treatment at North Star RTC with at least once a week family session.  The patient's representative and the patient both acknowledged that they understand all the medications prescribed and how to administer them, and the importance of the next level of care and continuing treatment.  If he experiences any side effects or has any concerns regarding his behavior, safety, immediately contact North Star Hospital.
 Keywords:
 discharge summary, adhd, adderall, attention deficit hyperactivity disorder, mood swings, odd, oppositional and defiant behavior, zoloft, adolescent, homicidal ideation, impulsive behavior, oppositional behavior, psychoeducational, psychotropic medications, oppositional and defiant, depressed, psychiatric, axis,
-</textarea>
+</textarea><p>
 
-        <input class="btn btn-info"  type="submit">
+        <input class="btn btn-info"  type="button" onclick="submitForm();" value="SUBMIT" name="submitButton">
+        
+        </p>
     </form>
+        <p>
+        <br>
+        <iframe src="cd.html" width="1100" height="600" frameborder="0" name="demoFrame"></iframe>
+    	</p>
+    	</center>
         </div>
         <p>&nbsp;</p>
+        
     <!-- /container -->
 <?php include_once dirname(__FILE__) . '/views/footer.php'; ?>
