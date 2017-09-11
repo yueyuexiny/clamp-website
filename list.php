180578720a1398c1454494816ccc7a7b574c9544
ftiryaki <firat.tiryaki@uth.tmc.edu>
2017-09-11 16:07:38 -0500

    list.php

--- list.php
+++ list.php
@@ -53,8 +53,8 @@ if ($conn->connect_error) {
 $count=0;
 $sql = "select * from $from $whereClause order by $field $thisSwitch";
 
-// $sqlDistinctUsers="select count(distinct(EMAIL)) $from $whereClause order by $field $thisSwitch";
-// var_dump($sqlDistinctUsers);
+$sqlDistinctUsers="select count(distinct(EMAIL)) $from $whereClause order by $field $thisSwitch";
+var_dump($sqlDistinctUsers);
 // $resDistinctUsers = $conn->query($sqlDistinctUsers);
 // $rowDistinctUsers = $resDistinctUsers->fetch_row();
 // $countDistinctUsers=$rowDistinctUsers[0];
