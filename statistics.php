10c4b62ba61d68646744d3bfa600ef53ed642c31
yueyuexiny <xhu7@localhost>
2019-03-18 11:44:09 -0500

    select all request and orgs

--- statistics.php
+++ statistics.php
@@ -7,8 +7,8 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-$organizationCountSQL = "select count(DISTINCT(ORGANIZATION)) as org from request_form, license WHERE request_form.ACTIVATION_CODE = license.sn and license.act_date is not NULL AND (CLAMP_VERSION IS NOT NULL) AND DOWNLOAD_TIME >= STR_TO_DATE('0000-00-00', '%Y-%m-%d') AND DOWNLOAD_TIME <= STR_TO_DATE('2050-00-00', '%Y-%m-%d') AND ISTEST=0";
-$userCountSQL = "select count(DISTINCT(EMAIL)) as usr from request_form, license WHERE request_form.ACTIVATION_CODE = license.sn and license.act_date is not NULL AND (CLAMP_VERSION IS NOT NULL) AND DOWNLOAD_TIME >= STR_TO_DATE('0000-00-00', '%Y-%m-%d') AND DOWNLOAD_TIME <= STR_TO_DATE('2050-00-00', '%Y-%m-%d') AND ISTEST=0";
+$organizationCountSQL = "select count(DISTINCT(ORGANIZATION)) as org from request_form";
+$userCountSQL = "select count(DISTINCT(EMAIL)) as usr from request_form";
 
 //sql-query: select DISTINCT(ORGANIZATION) as org from request_form, license WHERE request_form.ACTIVATION_CODE = license.sn and license.act_date is not NULL AND (CLAMP_VERSION IS NOT NULL) AND DOWNLOAD_TIME >= STR_TO_DATE('0000-00-00', '%Y-%m-%d') AND DOWNLOAD_TIME <= STR_TO_DATE('2050-00-00', '%Y-%m-%d') AND ISTEST=0 and lower(SYSTEM_USER) = 'yes';
 //select organizations with consent: "yes" for using there names.
