1b75e90b3d0bdc47d3b2694f3c019dd3e7db9bee
ftiryaki <firat.tiryaki@uth.tmc.edu>
2019-05-01 14:09:09 -0500

    cs

--- statistics.php
+++ statistics.php
@@ -8,7 +8,7 @@ if ($conn->connect_error) {
 }
 
 $organizationCountSQL = "select count(DISTINCT(ORGANIZATION)) as org from request_form where REQUEST_TIME is not null";
-$userCountSQL = "select count(DISTINCT(EMAIL)) as usr from request_form where REQUEST_TIME is not null";
+$userCountSQL = "select count(EMAIL) as usr from request_form where REQUEST_TIME is not null";
 $requestCountWithMonths = "SELECT MONTH(REQUEST_TIME) as month , YEAR(REQUEST_TIME) as year, COUNT(REQUEST_TIME) as count from request_form where REQUEST_TIME is not null GROUP BY MONTH(REQUEST_TIME),YEAR(REQUEST_TIME) ORDER BY YEAR(REQUEST_TIME),MONTH(REQUEST_TIME)"; 
 
 //sql-query: select DISTINCT(ORGANIZATION) as org from request_form, license WHERE request_form.ACTIVATION_CODE = license.sn and license.act_date is not NULL AND (CLAMP_VERSION IS NOT NULL) AND DOWNLOAD_TIME >= STR_TO_DATE('0000-00-00', '%Y-%m-%d') AND DOWNLOAD_TIME <= STR_TO_DATE('2050-00-00', '%Y-%m-%d') AND ISTEST=0 and lower(SYSTEM_USER) = 'yes';
