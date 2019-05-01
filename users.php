48267f75ff58e9aece48ac8ed4a64dff252d7d0e
ftiryaki <firat.tiryaki@uth.tmc.edu>
2019-05-01 13:14:14 -0500

    button

--- users.php
+++ users.php
@@ -51,10 +51,10 @@ td, th {
     <br><br>
     <div class="row">  
     <div class="col-md-6 col-sm-6 col-xs-6">  		
-  		<button type="button" style="padding: 2% 0;" class="btn btn-lg btn-primary btn-block active" role="button" aria-pressed="true"> <span style="font-size:40px;"><i style="color:orange;" class="fa fa-download" aria-hidden="true"></i>  <?php echo $userCount;?> Requests</span></button>
+  		<button type="button" style="padding: 2% 0;" class="btn btn-lg btn-primary btn-block active" role="button" aria-pressed="true"> <span style="font-size:30px;"><i style="color:orange;" class="fa fa-download" aria-hidden="true"></i>  <?php echo $userCount;?> Requests</span></button>
   		</div>
   		<div class="col-md-6 col-sm-6 col-xs-6"> 
-  		<button type="button" style="padding: 2% 0;" class="btn btn-lg btn-primary btn-block active" role="button" aria-pressed="true"><span style="font-size:40px;"><i style="color:orange;" class="fa fa-university" aria-hidden="true"></i>  <?php echo $organizationCount;?> Organizations</span></button>
+  		<button type="button" style="padding: 2% 0;" class="btn btn-lg btn-primary btn-block active" role="button" aria-pressed="true"><span style="font-size:30px;"><i style="color:orange;" class="fa fa-university" aria-hidden="true"></i>  <?php echo $organizationCount;?> Organizations</span></button>
   		</div>
  
           </div>
