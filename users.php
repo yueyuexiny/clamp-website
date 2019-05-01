19af476d8e24955728eb6dc7a6d6dd9e43387622
ftiryaki <firat.tiryaki@uth.tmc.edu>
2019-05-01 15:23:53 -0500

    cs

--- users.php
+++ users.php
@@ -54,7 +54,7 @@ td, th {
   		<button type="button" style="padding: 2% 0;" class="btn btn-lg btn-primary btn-block active" role="button" aria-pressed="true"> <span style="font-size:30px;"><i style="color:orange;" class="fa fa-download" aria-hidden="true"></i>  <?php echo $userCount;?> Requests</span></button>
   		</div>
   		
-  		<div class="col-md-6 col-sm-6 col-xs-6"> from </div>
+  		<div class="col-md-1 col-sm-1 col-xs-1"> from </div>
   		
   		<div class="col-md-6 col-sm-6 col-xs-6"> 
   		<button type="button" style="padding: 2% 0;" class="btn btn-lg btn-primary btn-block active" role="button" aria-pressed="true"><span style="font-size:30px;"><i style="color:orange;" class="fa fa-university" aria-hidden="true"></i>  <?php echo $organizationCount;?> Organizations</span></button>
